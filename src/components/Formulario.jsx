d22a13b39af6209d8b2e481de1f6e54fafd7c81c
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-05-17 11:39:15 -0300

    Se agregan los estados necesarios para el formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,12 +1,16 @@
 import {Button, Form} from 'react-bootstrap';
 import ListaTareas from './ListaTareas';
+import { useState } from 'react';
 
 const Formulario = () => {
+    const [tarea, setTarea] = useState('');
+    const [tareas, setTareas] = useState([]);
+
   return (
     <section>
       <Form>
         <Form.Group className="mb-3 d-flex" controlId="tarea">
-          <Form.Control type="text" placeholder="Ingrese una tarea" />
+          <Form.Control type="text" placeholder="Ingrese una tarea" onChange={(e)=>setTarea(e.target.value)} value={tarea}/>
         <Button variant="primary" type="submit" className='mx-1'>
           Agregar
         </Button>
