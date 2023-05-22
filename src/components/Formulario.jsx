9d6ccfa7c6adc0961fd9ca2f9919b88b7f92c638
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-05-22 10:22:43 -0300

    Agregamos tarea al array tareas

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -5,10 +5,16 @@ import { useState } from 'react';
 const Formulario = () => {
     const [tarea, setTarea] = useState('');
     const [tareas, setTareas] = useState([]);
-
+    //aqui creo las funciones
+    const handleSubmit = (e) =>{
+      e.preventDefault();
+      //crea un nuevo array con los datos que ya posee y agrega uno nuevo al final
+      setTareas([...tareas,tarea])
+      setTarea('');
+    }
   return (
     <section>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3 d-flex" controlId="tarea">
           <Form.Control type="text" placeholder="Ingrese una tarea" onChange={(e)=>setTarea(e.target.value)} value={tarea}/>
         <Button variant="primary" type="submit" className='mx-1'>
